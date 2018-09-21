3404191eee3b6213b476db7ed14d60940000222c
Jack McDade <jack@jackmcdade.com>
2018-09-21 11:24:23 -0400

    trim the base js/css to the bare minimum.

---
+++ resources/js/bootstrap.js
@@ -0,0 +1,4 @@
+// Bootstrap your JS here, if you need JS.
+
+// jQuery stub
+// window.$ = window.jQuery = require('jquery');
---
+++ resources/js/site.js
@@ -0,0 +1,19 @@
+/**
+ * First we will load all of this project's JavaScript dependencies.
+ * We make no assumptions as to what you need or want to use.
+ */
+
+require('./bootstrap');
+
+/**
+ * Next, if you want to use Vue.js, you can uncomment the following lines
+ * and install it with `npm install vue --save`
+ */
+
+// window.Vue = require('vue');
+//
+// Vue.component('example-component', require('./components/ExampleComponent.vue'));
+//
+// const site = new Vue({
+//     el: '#site'
+// });

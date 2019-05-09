9dea9274de3ea6ed19c0d5baf1e14850f1a5997e
Jason Varga <jason@pixelfear.com>
2019-05-08 21:46:57 -0400

    Add more directories, files, and please.

---
+++ config/statamic/api.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | API
+    |--------------------------------------------------------------------------
+    |
+    | Whether the API should be enabled, and through what route.
+    |
+    */
+
+    'enabled' => env('API_ENABLED', false),
+
+    'route' => env('API_ROUTE', 'api'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Middleware & Authentication
+    |--------------------------------------------------------------------------
+    |
+    | Define the middleware / middleware group that will be applied to the
+    | API route group. If you want to externally expose this API, here
+    | you can configure a middleware based authentication layer.
+    |
+    */
+
+    'middleware' => env('API_MIDDLEWARE', 'api'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Pagination
+    |--------------------------------------------------------------------------
+    |
+    | The numbers of items to show on each paginated page.
+    |
+    */
+
+    'pagination_size' => 50,
+
+];

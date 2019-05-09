998fe84498cd7564176923c3fda3102ff31a0ca2
Jack McDade <jack@jackmcdade.com>
2019-05-09 14:55:46 -0400

    :pencil: Updating the default routes and comments

--- config/statamic/routes.php
+++ config/statamic/routes.php
@@ -4,43 +4,12 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Collection Routes
-    |--------------------------------------------------------------------------
-    |
-    | Here you may define the route schema for the entries in each collection.
-    | You are able to add variables from the entries enclosed in curlies,
-    | as well as some special variables such as month, day, and year.
-    |
-    | More info: https://docs.statamic.com/routing#controllers
-    |
-    */
-
-    'collections' => [
-        // 'blog' => '/blog/{year}/{month}/{day}/{slug}',
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Taxonomy Routes
-    |--------------------------------------------------------------------------
-    |
-    | Here you may define the route schema for the terms in each taxonomy.
-    | The same rules apply here as the collection routes above.
-    |
-    */
-
-    'taxonomies' => [
-        // 'tags' => '/blog/tags/{slug}',
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
     | Routes
     |--------------------------------------------------------------------------
     |
     | Here you may define any template or controller based routes. Each route
-    | may contain wildcards, and can point to either a template name or an
-    | array containing all the data to be passed in to the template.
+    | may contain wildcards and can point to the name of a template or an
+    | array containing any data you want passed in to that template.
     |
     | More info: https://docs.statamic.com/routing
     |
@@ -49,7 +18,7 @@ return [
     'routes' => [
         '/' => [
             'template' => 'home',
-            'content' => 'Welcome home.',
+            'title' => 'Welcome to your new Statamic website.',
         ],
     ],
 
@@ -58,14 +27,14 @@ return [
     | Vanity Routes
     |--------------------------------------------------------------------------
     |
-    | A Vanity URL is a dummy, easy to remember URL that redirects you to a
-    | permanent URL. For example, a http://example.com/promo URL that may
-    | redirect you to http://example.com/blog/2016/09/this-months-promo
+    | Vanity URLs are easy to remember aliases that 302 redirect visitors to
+    | permanent URLs. For example, you can set https://example.com/hot-dogs
+    | to redirect to https://example.com/blog/2019/09/big-sale-on-hot-dogs.
     |
     */
 
     'vanity' => [
-        // '/promo' => '/blog/2017/09/this-months-promo',
+        // '/promo' => '/blog/2019/09/big-sale-on-hot-dogs',
     ],
 
     /*

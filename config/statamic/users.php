cbd868ad41f9f5e8769eb89d773c7b365f7e8ea8
Jason Varga <jason@pixelfear.com>
2019-08-29 13:47:09 -0400

    Update users.php, with the file repository as default

--- config/statamic/users.php
+++ config/statamic/users.php
@@ -1,18 +1,51 @@
 <?php
 
 return [
-    'driver' => 'file',
-    'redis_write_file' => true,
-    'login_type' => 'username',
-    'new_user_roles' => [],
-    'enable_gravatar' => true,
 
-    'roles' => [
-        'admin' => [
-            'title' => 'Admin',
-            'permissions' => [
-                'super',
+    /*
+    |--------------------------------------------------------------------------
+    | User Repository
+    |--------------------------------------------------------------------------
+    |
+    | Statamic uses a repository to get users, roles, groups, and their
+    | relationships from specified storage locations. The file driver
+    | gets it from disk, while the eloquent driver gets from a DB.
+    |
+    | Supported: "file", "eloquent"
+    |
+    */
+
+    'repository' => env('STATAMIC_USERS', 'file'),
+
+    'repositories' => [
+
+        'file' => [
+            'driver' => 'file',
+            'paths' => [
+                'users' => base_path('users'),
+                'roles' => config_path('statamic/user_roles.yaml'),
+                'groups' => config_path('statamic/user_groups.yaml'),
             ]
         ],
+
+        'eloquent' => [
+            'driver' => 'eloquent',
+        ],
+
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Avatars
+    |--------------------------------------------------------------------------
+    |
+    | User avatars are initials by default, with custom options for services
+    | like Gravatar.com.
+    |
+    | Supported: "initials", "gravatar", or a custom class name.
+    |
+    */
+
+    'avatars' => 'gravatar',
+
 ];

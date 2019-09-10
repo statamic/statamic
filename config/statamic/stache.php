684973e1a612d59e92c59ac9cd605a56fedeb1a8
Jason Varga <jason@pixelfear.com>
2019-09-10 09:24:19 -0400

    Update config

--- config/statamic/stache.php
+++ config/statamic/stache.php
@@ -4,12 +4,37 @@ use Statamic\Stache\Stores;
 
 return [
 
-    'update_every_request' => true,
+    /*
+    |--------------------------------------------------------------------------
+    | File Watcher
+    |--------------------------------------------------------------------------
+    |
+    | File changes will be noticed and data will be updated accordingly.
+    | This can be disabled to reduce overhead, but you will need to
+    | either update the cache manually or use the Control Panel.
+    |
+    */
+
+    'watcher' => true,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Stores
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure which stores are used inside the Stache.
+    |
+    */
 
     'stores' => [
 
         'taxonomies' => [
             'class' => Stores\TaxonomiesStore::class,
+            'directory' => base_path('content/taxonomies'),
+        ],
+
+        'terms' => [
+            'class' => Stores\TermsStore::class,
             'directory' => base_path('content/taxonomies'),
         ],
 
@@ -38,11 +63,26 @@ return [
             'directory' => base_path('content/assets'),
         ],
 
-        'users' => [
-            'class' => Stores\UsersStore::class,
-            'directory' => base_path('users'),
-        ],
+        // 'users' => [
+        //     'class' => Stores\UsersStore::class,
+        //     'directory' => base_path('users'),
+        // ],
 
-    ]
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Indexes
+    |--------------------------------------------------------------------------
+    |
+    | Here you may define any additional indexes that will be inherited
+    | by each store in the Stache. You may also define indexes on a
+    | per-store level by adding an "indexes" key to its config.
+    |
+    */
+
+    'indexes' => [
+        //
+    ],
 
 ];

37eba03a2f05e3047c754e13daab6a9e33a85fd3
Jason Varga <jason@pixelfear.com>
2020-10-02 15:01:57 -0400

    Laravel 8

--- config/auth.php
+++ config/auth.php
@@ -72,7 +72,7 @@ return [
 
         // 'users' => [
         //     'driver' => 'eloquent',
-        //     'model' => App\User::class,
+        //     'model' => App\Models\User::class,
         // ],
 
         // 'users' => [

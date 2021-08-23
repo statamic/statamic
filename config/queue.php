d46a73292c59fa587ce2a4add6f782ade1e13c95
Rias <hey@rias.be>
2021-08-23 13:43:30 +0200

    Set default table for logging failed jobs to `null`
    Statamic doesn't use a database by default, if you use queues but have no database, Laravel tries to put those failed jobs inside a database that doesn't exist.

--- config/queue.php
+++ config/queue.php
@@ -83,7 +83,7 @@ return [
     'failed' => [
         'driver' => env('QUEUE_FAILED_DRIVER', 'database'),
         'database' => env('DB_CONNECTION', 'mysql'),
-        'table' => 'failed_jobs',
+        'table' => null,
     ],
 
 ];

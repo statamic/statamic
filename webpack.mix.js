eb4fb210205428de3291bfb2f9260973955fddfc
Jack McDade <jack@jackmcdade.com>
2022-02-04 10:51:14 -0500

    Fix npm/webpack dependencies
    closes statamic/cms#5197

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,9 +14,8 @@ mix.js('resources/js/site.js', 'public/js')
 
 mix.postCss('resources/css/tailwind.css', 'public/css', [
     require('postcss-import'),
+    require('tailwindcss/nesting'),
     require('tailwindcss'),
-    require('postcss-nested'),
-    require('postcss-preset-env')({stage: 0})
 ])
 
 if (mix.inProduction()) {
@@ -36,7 +35,6 @@ if (mix.inProduction()) {
 // mix.js('resources/js/cp.js', 'public/vendor/app/js')
 //    .postCss('resources/css/cp.css', 'public/vendor/app/css', [
 //     require('postcss-import'),
+//     require('tailwindcss/nesting'),
 //     require('tailwindcss'),
-//     require('postcss-nested'),
-//     require('postcss-preset-env')({stage: 0})
 // ])

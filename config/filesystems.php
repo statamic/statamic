8b5083460c791a87dc91e6311bd863d795a398b6
Jesse Leite <jesseleite@gmail.com>
2022-02-08 15:45:52 -0500

    Put `visibility` back. It was never in Laravel's default config, but it's still a thing.

--- config/filesystems.php
+++ config/filesystems.php
@@ -51,6 +51,7 @@ return [
             'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            // 'visibility' => 'public', // https://statamic.dev/assets#visibility
         ],
 
         'assets' => [

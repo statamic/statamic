8d5e04480354a22901b2be9567e00f3c2ce63272
Alexander Stoffel <65236157+stoffelio@users.noreply.github.com>
2023-04-04 15:56:01 +0200

    Add *.antlers-php to tailwind config content array

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   content: [
     './resources/**/*.antlers.html',
+    './resources/**/*.antlers.php',
     './resources/**/*.blade.php',
     './resources/**/*.vue',
     './content/**/*.md'

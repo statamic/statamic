01840b0239c0641b93adb6104fecc62bce2e380c
Jason Varga <jason@pixelfear.com>
2023-05-04 12:43:09 -0400

    exclude and comment out cp stuff

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import vue2 from '@vitejs/plugin-vue2';
+// import vue2 from '@vitejs/plugin-vue2';
 
 export default defineConfig({
     plugins: [
@@ -8,11 +8,14 @@ export default defineConfig({
             input: [
                 'resources/css/site.css',
                 'resources/js/site.js',
-                'resources/css/cp.css',
-                'resources/js/cp.js',
+
+                // Control Panel assets.
+                // https://statamic.dev/extending/control-panel#adding-css-and-js-assets
+                // 'resources/css/cp.css',
+                // 'resources/js/cp.js',
             ],
             refresh: true,
         }),
-        vue2(),
+        // vue2(),
     ],
 });

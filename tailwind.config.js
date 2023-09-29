b1a301d73530b637b1cac8a71d1407869480b1d1
Ostap Brehin <osbre@protonmail.com>
2023-09-29 17:04:15 +0100

    Add types for Tailwind and format the config (#56)
    Co-authored-by: Jason Varga <jason@pixelfear.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,18 @@
-module.exports = {
-  content: [
-    './resources/**/*.antlers.html',
-    './resources/**/*.antlers.php',
-    './resources/**/*.blade.php',
-    './resources/**/*.vue',
-    './content/**/*.md'
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-}
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './resources/**/*.antlers.html',
+        './resources/**/*.antlers.php',
+        './resources/**/*.blade.php',
+        './resources/**/*.vue',
+        './content/**/*.md',
+    ],
+
+    theme: {
+        extend: {},
+    },
+
+    plugins: [
+        require('@tailwindcss/typography'),
+    ],
+};

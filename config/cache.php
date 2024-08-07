4e8757b62b89af29241ef1b6fe08a2043b48bd57
Duncan McClean <duncan@duncanmcclean.com>
2024-08-07 10:03:00 +0100

    Expose lock table name

--- config/cache.php
+++ config/cache.php
@@ -40,9 +40,10 @@ return [
 
         'database' => [
             'driver' => 'database',
+            'connection' => env('DB_CACHE_CONNECTION', null),
             'table' => env('DB_CACHE_TABLE', 'cache'),
-            'connection' => env('DB_CACHE_CONNECTION'),
             'lock_connection' => env('DB_CACHE_LOCK_CONNECTION'),
+            'lock_table' => env('DB_CACHE_LOCK_TABLE'),
         ],
 
         'file' => [

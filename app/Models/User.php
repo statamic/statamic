7e0cac013ee864c21dedbc0a2869b4733f839e51
Duncan McClean <duncan@duncanmcclean.com>
2024-11-20 16:34:59 +0000

    match HidesAttributes docblocks

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
     /**
      * The attributes that are mass assignable.
      *
-     * @var array<int, string>
+     * @var array<string>
      */
     protected $fillable = [
         'name',

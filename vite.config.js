6f19f56f0be0ed054a4581f1854755d5fbe7455d
Jack McDade <jack@jackmcdade.com>
2025-02-13 00:59:01 -0500

    Upgrade to TailwindCSS 4

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,33 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-// import vue2 from '@vitejs/plugin-vue2';
+import laravel from "laravel-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/css/site.css',
-                'resources/js/site.js',
-
-                // Control Panel assets.
-                // https://statamic.dev/extending/control-panel#adding-css-and-js-assets
-                // 'resources/css/cp.css',
-                // 'resources/js/cp.js',
-            ],
-            refresh: true,
-        }),
-        // vue2(),
-    ],
+export default defineConfig(({ command, mode }) => {
+    const env = loadEnv(mode, process.cwd(), "");
+    return {
+        build: {
+            rollupOptions: {
+                output: {
+                    manualChunks: (id) => {
+                        if (id.includes("node_modules"))
+                            return id
+                                .toString()
+                                .split("node_modules/")[1]
+                                .split("/")[0]
+                                .toString();
+                    },
+                },
+            },
+        },
+        plugins: [
+            tailwindcss(),
+            laravel({
+                refresh: true,
+                input: ["resources/css/site.css", "resources/js/site.js"],
+            }),
+        ],
+        server: {
+            open: env.APP_URL,
+        },
+    };
 });

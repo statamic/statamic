74efea110c2c1257e3f05ac7e90822f0077f4fdb
Jack McDade <jack@jackmcdade.com>
2025-02-25 15:07:58 -0500

    Upgrade to TailwindCSS 4 (#93)
    Co-authored-by: Jason Varga <jason@pixelfear.com>

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-// import vue2 from '@vitejs/plugin-vue2';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/site.css',
-                'resources/js/site.js',
-
-                // Control Panel assets.
-                // https://statamic.dev/extending/control-panel#adding-css-and-js-assets
-                // 'resources/css/cp.css',
-                // 'resources/js/cp.js',
-            ],
+            input: ['resources/css/site.css', 'resources/js/site.js'],
             refresh: true,
         }),
-        // vue2(),
+        tailwindcss(),
     ],
 });

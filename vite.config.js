514af115adf7c6489b9960260692a76e55569094
Jason Varga <jason@pixelfear.com>
2025-02-25 15:05:58 -0500

    nitpick to get it as identical to laravel/laravel as makes sense

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,13 @@
-import laravel from "laravel-vite-plugin";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig(({ command, mode }) => {
-    const env = loadEnv(mode, process.cwd(), "");
-    return {
-        plugins: [
-            tailwindcss(),
-            laravel({
-                refresh: true,
-                input: ["resources/css/site.css", "resources/js/site.js"],
-            }),
-        ],
-    };
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/site.css', 'resources/js/site.js'],
+            refresh: true,
+        }),
+        tailwindcss(),
+    ],
 });

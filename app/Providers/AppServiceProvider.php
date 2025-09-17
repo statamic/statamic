56afa1f31f521f3c00d5f3a0f92b7b2a9bb02134
Duncan McClean <duncan@duncanmcclean.com>
2025-09-17 15:39:15 +0100

    [6.x] Drop scaffolding for Control Panel assets (#109)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Statamic\Statamic;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,9 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // Statamic::vite('app', [
-        //     'resources/js/cp.js',
-        //     'resources/css/cp.css',
-        // ]);
+        //
     }
 }

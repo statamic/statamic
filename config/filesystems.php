ef4dc36f9398bc864e3bdd357a36ce8d5b4a2bb6
Laurens <62313434+laurenskr@users.noreply.github.com>
2025-10-07 11:55:14 +0200

    Fix: Docs link (#113)

--- config/filesystems.php
+++ config/filesystems.php
@@ -58,7 +58,7 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
             'report' => false,
-            // 'visibility' => 'public', // https://statamic.dev/assets#visibility
+            // 'visibility' => 'public', // https://statamic.dev/assets#container-visibility
         ],
 
         'assets' => [

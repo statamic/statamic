0609cddcdf056bc74f4666a7052c348b9ac5d192
Duncan McClean <duncan@duncanmcclean.com>
2026-03-17 17:48:50 +0100

    Sync with `laravel/laravel` (#132)

--- config/auth.php
+++ config/auth.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\User;
+
 return [
 
     /*
@@ -66,7 +68,7 @@ return [
 
         // 'users' => [
         //     'driver' => 'eloquent',
-        //     'model' => env('AUTH_MODEL', App\Models\User::class),
+        //     'model' => env('AUTH_MODEL', User::class),
         // ],
 
         // 'users' => [
